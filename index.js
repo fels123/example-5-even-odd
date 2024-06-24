f3a76825c8231db6604bcdfc576b7374e9370b57
fels123 <kelengulienakhro@gmail.com>
2024-06-24 18:38:24 +0530

    done

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(showNumbers(7));
+
+function showNumbers(limit) {
+  for (let i = 0; i <= limit; i++) {
+    if (i % 2 === 0) console.log(i, "Even");
+    else console.log(i, "Odd");
+  }
+}
